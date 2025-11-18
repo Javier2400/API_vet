362df32e9eaa85041520a150e20f21294d5f4b9c
Javier <st22030432@utlaguna.edu.mx>
2025-11-17 21:49:28 -0600

    apiecha

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("API de Veterinaria funcionando"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
